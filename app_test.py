b8eae019590a69a21a36ecdc5a2af025a3c9cb24
Rohan Gupta <rgupta@sevone.com>
2017-11-23 17:55:28 +0530

    Unit Test for Counties Validation

--- app_test.py
+++ app_test.py
@@ -7,9 +7,12 @@ class CitiesTestCase(unittest.TestCase):
 
   def test_index(self):
     tester = app.test_client(self)
-    response = tester.get('/cities.json', content_type='application/json')
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, json.dumps({"cities" : ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo", "Mumbai"]}))
+    response-city = tester.get('/cities.json', content_type='application/json')
+    self.assertEqual(response-city.status_code, 200)
+    self.assertEqual(response-city.data, json.dumps({"cities" : ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo", "Mumbai"]}))
+    response-countries = tester.get('/countries.json', content_type='application/json')
+    self.assertEqual(response-countries.status_code, 200)
+    self.assertEqual(response-countries.data, json.dumps({"cities" : ["Netherlands", "Germany", "US-East", "US-West", "Tokyo", "India"]}))
 
 if __name__ == '__main__':
     unittest.main()
